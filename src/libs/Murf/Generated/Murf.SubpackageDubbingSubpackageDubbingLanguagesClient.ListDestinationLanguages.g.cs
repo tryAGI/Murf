331894516f9d6c0de2a072e541e5a6fd48f1c2a1
Konstantin S. <havendv@gmail.com>
2026-03-29 01:20:35 +0400

    feat: Updated OpenAPI spec (#4)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Murf/Generated/Murf.SubpackageDubbingSubpackageDubbingLanguagesClient.ListDestinationLanguages.g.cs
+++ src/libs/Murf/Generated/Murf.SubpackageDubbingSubpackageDubbingLanguagesClient.ListDestinationLanguages.g.cs
@@ -253,7 +253,7 @@ namespace Murf
                     __response.EnsureSuccessStatusCode();
 
                     return
-                        global::System.Text.Json.JsonSerializer.Deserialize(__content, typeof(global::System.Collections.Generic.IList<global::Murf.LocaleResponse>), JsonSerializerContext) as global::System.Collections.Generic.IList<global::Murf.LocaleResponse> ??
+                        (global::System.Collections.Generic.IList<global::Murf.LocaleResponse>?)global::System.Text.Json.JsonSerializer.Deserialize(__content, typeof(global::System.Collections.Generic.IList<global::Murf.LocaleResponse>), JsonSerializerContext) ??
                         throw new global::System.InvalidOperationException($"Response deserialization failed for \"{__content}\" ");
                 }
                 catch (global::System.Exception __ex)
@@ -284,7 +284,7 @@ namespace Murf
                     ).ConfigureAwait(false);
 
                     return
-                        await global::System.Text.Json.JsonSerializer.DeserializeAsync(__content, typeof(global::System.Collections.Generic.IList<global::Murf.LocaleResponse>), JsonSerializerContext).ConfigureAwait(false) as global::System.Collections.Generic.IList<global::Murf.LocaleResponse> ??
+                        (global::System.Collections.Generic.IList<global::Murf.LocaleResponse>?)await global::System.Text.Json.JsonSerializer.DeserializeAsync(__content, typeof(global::System.Collections.Generic.IList<global::Murf.LocaleResponse>), JsonSerializerContext).ConfigureAwait(false) ??
                         throw new global::System.InvalidOperationException("Response deserialization failed.");
                 }
                 catch (global::System.Exception __ex)
--- src/libs/Murf/Generated/Murf.SubpackageDubbingSubpackageDubbingLanguagesClient.ListSourceLanguages.g.cs
+++ src/libs/Murf/Generated/Murf.SubpackageDubbingSubpackageDubbingLanguagesClient.ListSourceLanguages.g.cs
@@ -253,7 +253,7 @@ namespace Murf
                     __response.EnsureSuccessStatusCode();
 
                     return
-                        global::System.Text.Json.JsonSerializer.Deserialize(__content, typeof(global::System.Collections.Generic.IList<global::Murf.SourceLocaleResponse>), JsonSerializerContext) as global::System.Collections.Generic.IList<global::Murf.SourceLocaleResponse> ??
+                        (global::System.Collections.Generic.IList<global::Murf.SourceLocaleResponse>?)global::System.Text.Json.JsonSerializer.Deserialize(__content, typeof(global::System.Collections.Generic.IList<global::Murf.SourceLocaleResponse>), JsonSerializerContext) ??
                         throw new global::System.InvalidOperationException($"Response deserialization failed for \"{__content}\" ");
                 }
                 catch (global::System.Exception __ex)
@@ -284,7 +284,7 @@ namespace Murf
                     ).ConfigureAwait(false);
 
                     return
-                        await global::System.Text.Json.JsonSerializer.DeserializeAsync(__content, typeof(global::System.Collections.Generic.IList<global::Murf.SourceLocaleResponse>), JsonSerializerContext).ConfigureAwait(false) as global::System.Collections.Generic.IList<global::Murf.SourceLocaleResponse> ??
+                        (global::System.Collections.Generic.IList<global::Murf.SourceLocaleResponse>?)await global::System.Text.Json.JsonSerializer.DeserializeAsync(__content, typeof(global::System.Collections.Generic.IList<global::Murf.SourceLocaleResponse>), JsonSerializerContext).ConfigureAwait(false) ??
                         throw new global::System.InvalidOperationException("Response deserialization failed.");
                 }
                 catch (global::System.Exception __ex)
