2adb5869263f5124d4e1d42685da83cd9dcdab08
Konstantin S. <havendv@gmail.com>
2026-05-24 10:26:49 +0400

    feat: Updated OpenAPI spec (#27)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Murf/Generated/Murf.SubpackageAuthClient.GenerateToken.g.cs
+++ src/libs/Murf/Generated/Murf.SubpackageAuthClient.GenerateToken.g.cs
@@ -342,18 +342,17 @@ namespace Murf
                                     __exception_400 = __ex;
                                 }
 
-                                throw new global::Murf.ApiException<global::Murf.GenerateTokenResponse>(
+
+                                throw global::Murf.ApiException<global::Murf.GenerateTokenResponse>.Create(
+                                    statusCode: __response.StatusCode,
                                     message: __content_400 ?? __response.ReasonPhrase ?? string.Empty,
                                     innerException: __exception_400,
-                                    statusCode: __response.StatusCode)
-                                {
-                                    ResponseBody = __content_400,
-                                    ResponseObject = __value_400,
-                                    ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                    responseBody: __content_400,
+                                    responseObject: __value_400,
+                                    responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                         __response.Headers,
                                         h => h.Key,
-                                        h => h.Value),
-                                };
+                                        h => h.Value));
                             }
                             // Bad api-key provided
                             if ((int)__response.StatusCode == 401)
@@ -380,18 +379,17 @@ namespace Murf
                                     __exception_401 = __ex;
                                 }
 
-                                throw new global::Murf.ApiException<global::Murf.GenerateTokenResponse2>(
+
+                                throw global::Murf.ApiException<global::Murf.GenerateTokenResponse2>.Create(
+                                    statusCode: __response.StatusCode,
                                     message: __content_401 ?? __response.ReasonPhrase ?? string.Empty,
                                     innerException: __exception_401,
-                                    statusCode: __response.StatusCode)
-                                {
-                                    ResponseBody = __content_401,
-                                    ResponseObject = __value_401,
-                                    ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                    responseBody: __content_401,
+                                    responseObject: __value_401,
+                                    responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                         __response.Headers,
                                         h => h.Key,
-                                        h => h.Value),
-                                };
+                                        h => h.Value));
                             }
                             // Service Unavailable
                             if ((int)__response.StatusCode == 503)
@@ -418,18 +416,17 @@ namespace Murf
                                     __exception_503 = __ex;
                                 }
 
-                                throw new global::Murf.ApiException<global::Murf.GenerateTokenResponse3>(
+
+                                throw global::Murf.ApiException<global::Murf.GenerateTokenResponse3>.Create(
+                                    statusCode: __response.StatusCode,
                                     message: __content_503 ?? __response.ReasonPhrase ?? string.Empty,
                                     innerException: __exception_503,
-                                    statusCode: __response.StatusCode)
-                                {
-                                    ResponseBody = __content_503,
-                                    ResponseObject = __value_503,
-                                    ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                    responseBody: __content_503,
+                                    responseObject: __value_503,
+                                    responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                         __response.Headers,
                                         h => h.Key,
-                                        h => h.Value),
-                                };
+                                        h => h.Value));
                             }
 
                             if (__effectiveReadResponseAsString)
@@ -463,17 +460,15 @@ namespace Murf
                                 }
                                 catch (global::System.Exception __ex)
                                 {
-                                    throw new global::Murf.ApiException(
+                                    throw global::Murf.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
                             else
@@ -510,17 +505,15 @@ namespace Murf
                                     {
                                     }
 
-                                    throw new global::Murf.ApiException(
+                                    throw global::Murf.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
 
